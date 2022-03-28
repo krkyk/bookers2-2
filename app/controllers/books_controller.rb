45acb10146349984547485268c687316b22528bb
EC2 Default User <ec2-user@ip-172-31-91-227.ec2.internal>
2022-03-28 06:29:28 +0000

    [Finish]task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
     @book.user_id=current_user.id
     @user=User.find(current_user.id)
     if @book.save
+      flash[:success]="You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @books=Book.all
@@ -25,11 +26,17 @@ class BooksController < ApplicationController
 
   def edit
     @book=Book.find(params[:id])
+    if @book.user==current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
     @book=Book.find(params[:id])
     if @book.update(book_params)
+      flash[:success]="You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
